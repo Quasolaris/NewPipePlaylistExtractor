79fb31385ccad9adecb505dad5562f29f74d1ded
Sebastian Grau <sebastian.grau@students.fhnw.ch>
2022-03-18 12:08:38 +0100

    Feat: Extraction and Download

---
+++ Script/main.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+
+
+
+from io import StringIO
+from sqlite3 import Error
+import csv
+import sqlite3
+import sys
+import os
+import requests
+from pytube import YouTube
+
+
+#===== Database extract SQlite by rachmadaniHaryono, found on comment: https://github.com/TeamNewPipe/NewPipe/issues/1788#issuecomment-500805819
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+
+def get_rows(db_file):
+    conn = create_connection(db_file)
+
+    sqlCmds = """
+    select 
+            url,
+            title,
+            playlists.name as playlist_name
+    from streams 
+    inner join playlist_stream_join on playlist_stream_join.stream_id = streams.uid
+    inner join playlists on playlists.uid == playlist_stream_join.playlist_id
+    """
+    cur = conn.cursor()
+    cur.execute(sqlCmds)
+    rows = cur.fetchall()
+    return rows
+#=====
+
+def getPlaylists(db_file):
+    print("Extracting Playlists...")
+    rows = get_rows(db_file)
+    
+    """
+    Sorting playlists
+    Dictionary has playlist name as key 
+    and the list of URLs as Value
+
+    Folder gets named after Key, and URLs
+    downloaded into given folder
+    
+    TODO: Add meta data to songs --> Playlist name as Album
+    """
+
+    PlaylistDir = {"": ""}
+    for row in rows:
+        PlaylistDir[row[2]] = []
+
+    for row in rows:
+        PlaylistDir[row[2]] += [row[0]]
+
+    return PlaylistDir
+
+# https://www.geeksforgeeks.org/download-video-in-mp3-format-using-pytube/   
+def downloadPlaylist(folderName, playlist):
+
+    path = "./Playlists/" + folderName
+
+    if(not os.path.exists(path)):
+        os.mkdir("./Playlists/" + folderName)
+
+    # download audio
+    for videoURL in playlist:
+        
+        if(checkIfAvaiable):
+            
+            print("Downloading: " + videoURL)
+            try:
+                YouTubeVideo = YouTube(str(videoURL))
+
+                songName = YouTubeVideo.streams[0].title
+                destination = path + "/"
+
+            
+                audio = YouTubeVideo.streams.filter(only_audio=True).first()
+                audioFile = audio.download(output_path=destination)
+
+                base, ext = os.path.splitext(audioFile)
+                new_file = base + '.mp3'
+                os.rename(audioFile, new_file)
+            except  Exception as e: 
+                print(e)
+                
+
+
+        else:
+            print(videoURL + " not avaiable in your location (Try a VPN)")
+
+    
+
+# taken from S P Sharan: https://stackoverflow.com/questions/68818442/how-to-check-if-a-youtube-video-exists-using-python
+def checkIfAvaiable(url):
+    pattern = '"playabilityStatus":{"status":"ERROR","reason":"Video unavailable"'
+    
+    request = requests.get(url)
+    return False if pattern in request.text else True
+
+def main(db_file):
+    
+    Playlists = getPlaylists(db_file)
+
+    playlistCount = len(Playlists)
+
+    print(str(playlistCount) + " Playlists extracted: ")
+
+    for key in Playlists:
+        print("=> "+ key)
+
+    downloadPlaylist("Test", Playlists['Poshig'])
+
+if __name__ == '__main__':
+    main(sys.argv[1])
